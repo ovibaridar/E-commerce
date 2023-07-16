ecf1b61ba4ffcee07e5a0209d25aea8946d0d457
ovibaridar <ovibaridar06@gmail.com>
2023-07-16 21:56:19 +0600

    new project

--- E_co/shop/urls.py
+++ E_co/shop/urls.py
@@ -3,5 +3,10 @@ from .import views
 
 urlpatterns = [
 
-    path('', views.shophome, name='shop')
+    path('', views.shophome, name='shop'),
+    path('productview', views.productview, name='view'),
+    path('acount', views.acount, name='acount'),
+    path('search', views.search, name='search'),
+    path('about', views.about, name='about'),
+    path('chakeout', views.chakeout, name='chakeout'),
 ]
--- E_co/shop/views.py
+++ E_co/shop/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def shophome(request):
-    return render(request,'shop/shop.html')
+    return render(request, 'shop/shop.html')
+
+def about(request):
+
+    return render(request, 'shop/about.html')
+
+def acount(request):
+
+    return render(request, 'shop/acount.html')
+
+def chakeout(request):
+
+    return render(request, 'shop/chakeout.html')
+
+def productview(request):
+
+    return render(request, 'shop/productview.html')
+
+def search(request):
+    return render(request, 'shop/search.html')
