ee03871504e55aef246d5b490e4231a603e2bd4c
ovibaridar <ovibaridar06@gmail.com>
2023-07-21 14:12:14 +0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	E_co/shop/templates/shop/hello.html

--- E_co/shop/urls.py
+++ E_co/shop/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path ,include
 from .import views
+from .import about
+
 
 urlpatterns = [
 
     path('', views.shophome, name='shophome'),
+    path('about', about.about, name='about')
+
     # path('E_co/', include('E_co.urls'))
 
 ]
--- E_co/shop/views.py
+++ E_co/shop/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponseGone
-
+from .models import product
 # Create your views here.
 def shophome(reuest):
+
+    all_products = product.objects.all()
+
     # return HttpResponseGone ("gu")
-    return render(reuest,'shop/hello.html')
+    return render(reuest,'shop/hello.html', {'products': all_products})
+
+
+
+
+
+
+
