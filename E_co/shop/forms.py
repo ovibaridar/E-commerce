b597a16c24f1e794c44664d687d26a12e3c264f6
ovibaridar <ovibaridar06@gmail.com>
2023-07-24 23:30:51 +0600

    update  my  new .
    # Conflicts:
    #	E_co/shop/templates/shop/hello.html

---
+++ E_co/shop/forms.py
@@ -0,0 +1,9 @@
+
+from django import forms
+
+class MyForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
+    second_name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(max_length=100, required=True)
+    phone = forms.CharField(max_length=11, min_length=11, required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
--- E_co/shop/qr.py
+++ E_co/shop/qr.py
@@ -1,4 +1,24 @@
+
 from django.shortcuts import render
+from django.http import HttpResponseGone
+from .forms import MyForm
 
 def formqr(request):
-    return render(request , 'shop/qr.html')
+    if request.method == 'POST':
+        form = MyForm(request.POST)
+        if form.is_valid():
+            # Process the form data
+            name = form.cleaned_data['name']
+            second_name = form.cleaned_data['second_name']
+            email = form.cleaned_data['email']
+            phone = form.cleaned_data['phone']
+            message = form.cleaned_data['message']
+
+            # Do something with the form data (e.g., save to database)
+
+            # Redirect to a success page
+            return HttpResponseGone("ok")
+    else:
+        form = MyForm()
+
+    return render(request, 'shop/qr.html', {'form': form})
