3b5623feba9554e67ca686996133d55c0f26635a
ovibaridar <ovibaridar06@gmail.com>
2023-07-26 12:07:24 +0600

    update newly
    # Conflicts:
    #	E_co/shop/templates/shop/hello.html

--- E_co/shop/views.py
+++ E_co/shop/views.py
@@ -1,23 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponseGone
 from .models import product
-from qrcode import*
+from qrcode import *
+
+
 # import qrcode
 # Create your views here.
 def shophome(request):
-
     all_products = product.objects.all()
+    num_products_per_set = 4
 
+    # Group the products into sets of 4
+    grouped_products = [all_products[i:i + num_products_per_set] for i in
+                        range(0, len(all_products), num_products_per_set)]
 
-    # return HttpResponseGone ("gu")
-    return render(request,'shop/hello.html', {'products': all_products })
+    return render(request, 'shop/hello.html', {'grouped_products': grouped_products})
 
 
 def genarator(request):
-
-    return render(request,'shop/Qrcode.html')
-
-
+    return render(request, 'shop/Qrcode.html')
 
 
 def qrcode(request):
@@ -30,10 +31,5 @@ def qrcode(request):
         return render(request, 'shop/Qrcode.html', proms)
     img = make(data)
     img.save('shop/static/qrimg/Qrimg.jpg')
-    proms = { 'img': img}
+    proms = {'img': img}
     return render(request, 'shop/Qrcode.html', proms)
-
-
-
-
-
