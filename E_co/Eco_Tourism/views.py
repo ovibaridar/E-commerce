0670d86a541c20cc5f172bb12cb17677e1c848fc
ovibaridar <ovibaridar06@gmail.com>
2023-08-03 00:34:45 +0600

    eco-tourism 2023--->2/8/2023 # Conflicts:

--- E_co/Eco_Tourism/views.py
+++ E_co/Eco_Tourism/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def EcoHome(request):
-    return render(request,'Eco_Tourism/Home page.html')
+    par={'active':"active"}
+    return render(request,'Eco_Tourism/Home page.html',par)
