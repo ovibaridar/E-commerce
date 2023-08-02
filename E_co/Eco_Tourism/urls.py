3e4eb6172ead42dcd1f228ceb45afcc08fc47506
ovibaridar <ovibaridar06@gmail.com>
2023-08-03 04:51:32 +0600

    eco-tourism 2023--->2/8/2023 # Conflicts:

--- E_co/Eco_Tourism/urls.py
+++ E_co/Eco_Tourism/urls.py
@@ -3,5 +3,15 @@ from .import views
 
 urlpatterns = [
 
-    path('', views.EcoHome, name='home')
+    path('', views.EcoHome, name='home'),
+    path('eco_tours', views.eco_tours, name='eco_tours'),
+    path('eco_hotels', views.eco_hotels, name='eco_hotels'),
+    path('Travel_Tips', views.Travel_Tips, name='Travel_Tips'),
+    path('Photo_and_Videos', views.Photo_and_Videos, name='Photo_and_Videos'),
+    path('Sustainability', views.Sustainability, name='Sustainability'),
+    path('Booking', views.Booking, name='Booking'),
+    path('Testimonials', views.Testimonials, name='Testimonials'),
+    path('About_Us', views.About_Us, name='About_Us'),
+    path('Contact', views.Contact, name='Contact'),
+    path('blog', views.blog, name='blog')
 ]
--- E_co/Eco_Tourism/views.py
+++ E_co/Eco_Tourism/views.py
@@ -1,8 +1,55 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 def EcoHome(request):
-    par={'active':"active"}
-    return render(request,'Eco_Tourism/Home page.html',par)
+    par = {'home_active': "active" ,'range':range(1, 10)}
+    return render(request, 'Eco_Tourism/Home page.html', par)
+
+
+def blog(request):
+    par = {'blog_active': "active"}
+    return render(request, 'Eco_Tourism/blog.html', par)
+
+
+def eco_tours(request):
+    par = {'eco_tours_active': "active"}
+    return render(request, 'Eco_Tourism/eco_tours.html', par)
+
+
+def eco_hotels(request):
+    par = {'hotels_active': "active"}
+    return render(request, 'Eco_Tourism/hotels.html', par)
+
+
+
+# dropdwons
+
+
+
+def Travel_Tips(request):
+    return render(request, 'Eco_Tourism/Travel Tips.html')
+
+
+def Photo_and_Videos(request):
+    return render(request, 'Eco_Tourism/Photo and Videos.html')
+
+
+def Sustainability(request):
+    return render(request, 'Eco_Tourism/Sustainability.html')
+
+
+def Booking(request):
+    return render(request, 'Eco_Tourism/Booking.html')
+
+
+def Testimonials(request):
+    return render(request, 'Eco_Tourism/Testimonials.html')
+
+
+def About_Us(request):
+    return render(request, 'Eco_Tourism/About_Us.html')
+def Contact(request):
+    return render(request, 'Eco_Tourism/Contact.html')
